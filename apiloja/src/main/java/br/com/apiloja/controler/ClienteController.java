0243a9615261e913b1f7e09158e7c892962b68fb
imsilva100 <ismael@sonner.com.br>
2022-08-09 15:29:42 -0300

    correções novas

--- apiloja/src/main/java/br/com/apiloja/controler/ClienteController.java
+++ apiloja/src/main/java/br/com/apiloja/controler/ClienteController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.apiloja.modelo.Cliente;
 import br.com.apiloja.repository.ClienteRepository;
 
